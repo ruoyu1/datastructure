d4176dd900886d54175dbc6fdb4798b3282f863a
liuruoyu <liuruoyu@gz.sia.cn>
2025-08-06 14:19:26 +0800

    update； 删除重复的字母

---
+++ binarytree/src/main/java/code/basic/datastructure/removeRepeated/RemoveRepeated.java
@@ -0,0 +1,49 @@
+package code.basic.datastructure.removeRepeated;
+
+import java.util.*;
+
+public class RemoveRepeated {
+    private Character currentCharacter;
+    private Integer count;
+    private Deque<Character> characters = new LinkedList<>();
+    private Stack<Integer>  countHistory = new Stack<>();
+
+    public String removeRepeated(String str) {
+        for (Character c : str.toCharArray()
+        ) {
+            if(Objects.isNull(currentCharacter)){
+                currentCharacter = c;
+                count = 0;
+            }
+            if(c.equals(currentCharacter)){
+                count = count + 1;
+                if(count >= 3){
+                    characters.removeLast();
+                    characters.removeLast();
+                    if(characters.size()>0) {
+                        currentCharacter = characters.peekLast();
+                        count=countHistory.pop();
+                    }else {
+                        currentCharacter = null;
+                    }
+                }else{
+                    characters.add(c);
+                }
+            }else{
+                currentCharacter = c;
+                countHistory.add(count);
+                characters.add(c);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!characters.isEmpty()){
+            stringBuilder.append(characters.pop());
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String re = new RemoveRepeated().removeRepeated("aabcccbbad");
+        System.out.println(re);
+    }
+}
